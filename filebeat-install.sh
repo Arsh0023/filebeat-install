ea6bdbc191a6ceae76c0d43a1acdb9bf95ef676b
Arshpreet Singh <arsharsh2391@gmail.com>
2024-01-15 16:58:33 +0530

    Update filebeat-install.sh

--- filebeat-install.sh
+++ filebeat-install.sh
@@ -2,7 +2,13 @@
 
 #ps aux | grep -E 'filebeat' | awk '{print $2}' | while read -r l; do kill -9 $l;done
 cd /home/ec2-user || { echo "Error: Unable to change to the home directory."; exit 1; }
-wget https://devops-tookan.s3.ap-south-1.amazonaws.com/filebeatInstall.zip || { echo "Error: Unable to download Filebeat installation zip."; exit 1; }
+
+#pubIp
+#wget https://devops-tookan.s3.ap-south-1.amazonaws.com/filebeatInstall.zip || { echo "Error: Unable to download Filebeat installation zip."; exit 1; }
+
+#privIP
+wget https://devops-tookan.s3.ap-south-1.amazonaws.com/filebeatPriv.zip || { echo "Error: Unable to download Filebeat installation zip."; exit 1; }
+
 unzip filebeatInstall.zip || { echo "Error: Unable to unzip the Filebeat installation zip."; exit 1; }
 cd filebeat-8.11.4-linux-x86_64 || { echo "Error: Unable to change to the Filebeat directory."; exit 1; }
 #screen -S filebeat-screen || { echo "Error: Unable to create a screen session for Filebeat."; exit 1; }
